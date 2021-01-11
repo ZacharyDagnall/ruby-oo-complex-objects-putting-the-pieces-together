5c1b2733267388be88eb402442c161adf65e8790
ZacharyDagnall <zachary.dagnall@gmail.com>
2021-01-11 17:48:33 -0500

    commit tree 9e0820d7ac1407c94a3d5641eed0b39d6786b08b

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,13 @@
+class Book
+    def initialize(given_title)
+        @title = given_title
+    end
 
+    attr_accessor :title, :author, :page_count, :genre
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+end
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,15 @@
 # Make your shoe class here!
+class Shoe
+    def initialize(given_brand)
+        @brand = given_brand
+    end
+
+    attr_accessor :brand, :color, :size, :material, :condition
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end
+
+end
+
